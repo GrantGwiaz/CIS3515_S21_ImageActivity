8c62c0b9623399e7a6cd955f2a4f82ebfc485890
GrantGwiaz <61506873+GrantGwiaz@users.noreply.github.com>
2021-02-21 23:18:24 -0500

    Add steOnItemCLickListener for spinner, showPicture()

--- app/src/main/java/edu/temple/myapplication/ImageActivity.java
+++ app/src/main/java/edu/temple/myapplication/ImageActivity.java
@@ -3,8 +3,11 @@ package edu.temple.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class ImageActivity extends AppCompatActivity {
 
@@ -23,15 +26,37 @@ public class ImageActivity extends AppCompatActivity {
         spinner = findViewById(R.id.spinner);
         imageView = findViewById(R.id.imageView);
 
-        soupArray = new String[]{"","Gazpacho", "French Onion Soup", "Chowder", "Tomato Soup"};
+        soupArray = new String[]{"Pleas select a Soup","Gazpacho", "French Onion Soup", "Chowder", "Tomato Soup"};
         descriptionArray = new String[]{"","a cold soup of raw blended vegetables", "a rich soup typically containing fish",
                                     "beef stock and onion base, covered with cheese and bread", "pretty much just tomatoes"};
         soupImagesArray = new int[]{R.drawable.gazpacho , R.drawable.frenchOnion , R.drawable.chowder , R.drawable.tomato};
 
-        //spinner.setAdapter();
+        ImageAdapter adapter = new ImageAdapter(this, soupArray, descriptionArray, soupImagesArray);
+        spinner.setAdapter(adapter);
+
+
+        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    Toast.makeText(ImageActivity.this, "Soup Selected", Toast.LENGTH_SHORT).show();
+                    showPicture(position);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    Toast.makeText(ImageActivity.this, "Item Unselected", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void showPicture (int position) {
-        imageView.setImageResource(soupImagesArray[position]);
+        if(position > 0) {
+            imageView.setImageResource(soupImagesArray[position]);
+        } else {
+            imageView.setImageResource(android.R.color.transparent);
+        }
     }
 }
