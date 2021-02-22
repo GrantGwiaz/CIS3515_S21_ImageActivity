8f820e19a551f6aa6ac823bd6016944625c059df
GrantGwiaz <61506873+GrantGwiaz@users.noreply.github.com>
2021-02-21 22:39:53 -0500

    Add Arrays containing Soup names, descriptions, and drawable ints

--- app/src/main/java/edu/temple/myapplication/ImageActivity.java
+++ app/src/main/java/edu/temple/myapplication/ImageActivity.java
@@ -3,12 +3,35 @@ package edu.temple.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class ImageActivity extends AppCompatActivity {
 
+    ImageView imageView;
+    Spinner spinner;
+
+
+    int[] soupImagesArray;
+    String[] soupArray;
+    String[] descriptionArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        imageView = findViewById(R.id.imageView);
+
+        soupArray = new String[]{"","Gazpacho", "French Onion Soup", "Chowder", "Tomato Soup"};
+        descriptionArray = new String[]{"","a cold soup of raw blended vegetables", "a rich soup typically containing fish",
+                                    "beef stock and onion base, covered with cheese and bread", "pretty much just tomatoes"};
+        soupImagesArray = new int[]{R.drawable.gazpacho , R.drawable.frenchOnion , R.drawable.chowder , R.drawable.tomato};
+
+        //spinner.setAdapter();
+    }
+
+    private void showPicture (int position) {
+        imageView.setImageResource(soupImagesArray[position]);
     }
 }
--- app/src/main/java/edu/temple/myapplication/ImageAdapter.java
+++ app/src/main/java/edu/temple/myapplication/ImageAdapter.java
@@ -1,5 +1,6 @@
 package edu.temple.myapplication;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -36,8 +37,6 @@ public class ImageAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         TextView textView = new TextView(context);
         textView.setText(items.get(position));
-
-        textView.setPadding(10, 8, 8);
 
         return textView;
     }
