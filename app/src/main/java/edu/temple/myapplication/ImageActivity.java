81cb78c1a85ff1bc0a2f9e0d8c4e316cbe7140d1
GrantGwiaz <61506873+GrantGwiaz@users.noreply.github.com>
2021-02-22 22:18:15 -0500

    Spinner displays Item names

--- app/src/main/java/edu/temple/myapplication/ImageActivity.java
+++ app/src/main/java/edu/temple/myapplication/ImageActivity.java
@@ -23,38 +23,39 @@ public class ImageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // get our views so we can use them
         spinner = findViewById(R.id.spinner);
         imageView = findViewById(R.id.imageView);
 
-        soupArray = new String[]{"Pleas select a Soup","Gazpacho", "French Onion Soup", "Chowder", "Tomato Soup"};
+        //declare and populate arrays for each for our items, their descriptions, and images of them to be displayed
+        soupArray = new String[]{"Please select a Soup","Gazpacho", "French Onion Soup", "Chowder", "Tomato Soup"};
         descriptionArray = new String[]{"","a cold soup of raw blended vegetables", "a rich soup typically containing fish",
                                     "beef stock and onion base, covered with cheese and bread", "pretty much just tomatoes"};
-        soupImagesArray = new int[]{R.drawable.gazpacho , R.drawable.frenchOnion , R.drawable.chowder , R.drawable.tomato};
+        soupImagesArray = new int[]{R.drawable.gazpacho , R.drawable.french_onion , R.drawable.chowder , R.drawable.tomato};
+        // all this is better as an object class as to encapsulate, but since won't be using or maintaining this its unnecessary
+
 
         ImageAdapter adapter = new ImageAdapter(this, soupArray, descriptionArray, soupImagesArray);
         spinner.setAdapter(adapter);
 
-
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //spinner listener
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                @Override
-                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    Toast.makeText(ImageActivity.this, "Soup Selected", Toast.LENGTH_SHORT).show();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ImageActivity.this, "Soup Selected", Toast.LENGTH_SHORT).show();
                     showPicture(position);
                 }
-
-                @Override
-                public void onNothingSelected(AdapterView<?> parent) {
-                    Toast.makeText(ImageActivity.this, "Item Unselected", Toast.LENGTH_SHORT).show();
-                }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(ImageActivity.this, "Item Unselected", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    //method for showing the picture based on the selected item from spinner
     private void showPicture (int position) {
         if(position > 0) {
-            imageView.setImageResource(soupImagesArray[position]);
+            imageView.setImageResource(soupImagesArray[position-1]);
         } else {
             imageView.setImageResource(android.R.color.transparent);
         }
