098a895fbc9c014cd7d1576addf087c6e805f6c0
GrantGwiaz <61506873+GrantGwiaz@users.noreply.github.com>
2021-02-23 00:01:19 -0500

    Override getDropDownView in Spinner, Spinner does not show image when selected, formatting

--- app/src/main/java/edu/temple/myapplication/ImageAdapter.java
+++ app/src/main/java/edu/temple/myapplication/ImageAdapter.java
@@ -1,9 +1,13 @@
 package edu.temple.myapplication;
 
 import android.content.Context;
+import android.media.Image;
+import android.util.Pair;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -23,13 +27,12 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     @Override
-    public int getCount() {
-        return items.length;
-    }
+    public int getCount() { return items.length; }
 
     @Override
     public Object getItem(int position) {
-        return items[position];
+        Pair<String, String> soup = new Pair(items[position], descriptions[position]);
+        return soup;
     }
 
     @Override
@@ -39,8 +42,73 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView textView = new TextView(context);
-        textView.setText(items[position]);
-        return textView;
+
+        TextView soupName, soupDescription;
+
+        LinearLayout linearLayout;
+
+        if(convertView == null) {
+            linearLayout = new LinearLayout(context);
+            soupName = new TextView(context);
+            soupDescription = new TextView(context);
+
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            linearLayout.addView(soupName);
+            linearLayout.addView(soupDescription);
+
+            soupName.setTextSize(20);
+            soupName.setPadding(30, 30,15,0);
+            soupDescription.setPadding(15, 30,15,30);
+
+        } else {
+            linearLayout = (LinearLayout) convertView;
+            soupName = (TextView) linearLayout.getChildAt(0);
+            soupDescription = (TextView) linearLayout.getChildAt(1);
+        }
+        soupName.setText(items[position]);
+        soupDescription.setText(descriptions[position]);
+
+        return linearLayout;
+    }
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        TextView soupName, soupDescription;
+        ImageView imageView;
+
+        LinearLayout linearLayout;
+
+        if(convertView == null) {
+            linearLayout = new LinearLayout(context);
+            soupName = new TextView(context);
+            soupDescription = new TextView(context);
+            imageView = new ImageView(context);
+
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            linearLayout.addView(imageView);
+            linearLayout.addView(soupName);
+            linearLayout.addView(soupDescription);
+
+            soupName.setTextSize(15);
+            soupName.setPadding(15, 30,15,0);
+            soupDescription.setPadding(15, 30,15,30);
+
+            imageView.getLayoutParams().height = 150;
+            imageView.getLayoutParams().width = 150;
+
+        } else {
+            linearLayout = (LinearLayout) convertView;
+            imageView = (ImageView) linearLayout.getChildAt(0);
+            soupName = (TextView) linearLayout.getChildAt(1);
+            soupDescription = (TextView) linearLayout.getChildAt(2);
+        }
+        if(position > 0) {
+            imageView.setImageResource(imageResources[position - 1]);
+        } else {
+            imageView.setImageResource(android.R.color.transparent);
+        }
+        soupName.setText(items[position]);
+        soupDescription.setText(descriptions[position]);
+
+        return linearLayout;
     }
 }
